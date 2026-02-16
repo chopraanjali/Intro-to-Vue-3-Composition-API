dfd76dce0089b7b39311b1dd55cab5980ef49406
Anjali Chopra <anjalichopra1994@gmail.com>
2026-02-16 18:19:10 +0530

    added more conditionals

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
+const inventory = ref(5)
+const onSale = ref(true)
 
 </script>
   
@@ -16,6 +18,10 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="inventory > 10">In stock</p>
+        <p v-else-if="inventory >0 && inventory <= 10">Only few items left!</p>
+        <p v-else>Out of stock</p>
+        <p v-if="onSale">On Sale!</p>
       </div>
     </div>
   </div>
