858942298fcc3dbc17fc010010edb58491305926
Anjali Chopra <anjalichopra1994@gmail.com>
2026-02-16 18:50:00 +0530

    added mouse over effects and cart decrementer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
@@ -9,11 +10,23 @@ const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage },
 ])
 
 const cart = ref(0)
+const addToCart = () => {
+  cart.value++
+}
+const removeFromCart = () => {
+  if (cart.value > 0) {
+    cart.value--
+  }
+}
+
+const updateImage = (variantImage) => {
+  image.value = variantImage
+}
 </script>
   
 <template>
@@ -33,11 +46,13 @@ const cart = ref(0)
         </ul>
         <div
           v-for="variant in variants"
-          :key="variant.id"
+          :key="variant.id" 
+          @mouseover="updateImage(variant.image)"
         >
           {{ variant.color }}
         </div>
-        <button class="button">Add to Cart</button>
+        <button class="button" v-on:click="addToCart">Add to Cart</button>
+        <button class="button" v-on:click="removeFromCart">Remove from Cart</button>
       </div>
     </div>
   </div>
