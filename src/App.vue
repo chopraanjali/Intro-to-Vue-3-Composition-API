34042cd76a5e5571879bc3caf01477ebe2b8d173
Anjali Chopra <anjalichopra1994@gmail.com>
2026-02-16 17:40:56 +0530

    dynamic product name using ref

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue' //state management
+// ref is a function that takes an initial value and returns a reactive reference to that value.
+
+const product = ref('Socks')
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product }}</h1>
       </div>
     </div>
   </div>
